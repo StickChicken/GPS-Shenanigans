ea445101d8287b5a7993e785614dcfa8d14edc14
StickChicken <hailsauron@gmail.com>
2021-11-14 15:55:04 -0500

    misc stuff

--- GPS.ino
+++ GPS.ino
@@ -1,7 +1,15 @@
-void setup(){ 
+#include <SoftwareSerial.h>
 
+SoftwareSerial ss(4,3);
+
+void setup(){ 
+   Serial.begin(9600);
+   ss.begin(9600);
 }
 
 void loop(){
-
+   while(ss.available() > 0){
+	byte gpsData = ss.read();
+	Serial.write(gpsData);
+   }
 }
